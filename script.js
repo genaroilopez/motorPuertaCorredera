b828d19edd08bffbcfb6eb3f29c3e85e84a1dd30
genaroilopez <122406875+genaroilopez@users.noreply.github.com>
2023-01-15 17:22:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,64 @@ let boxCategorias1 = document.getElementById('contenedorCategorias1');
 let categoria2 = document.getElementById('categoria2');
 let boxCategorias2 = document.getElementById('contenedorCategorias2');
 
+
+let menu = document.getElementById('menuMovil');
+let botonMenu = document.getElementById('menu');
+let fondo = document.getElementById('fondo');
+
+let botonPesos = document.getElementById('botonPesos');
+let categoriaPesos = document.getElementById('categoriaPesos');
+let botonMarcas = document.getElementById('botonMarcas');
+let categoriaMarcas = document.getElementById('categoriaMarcas');
+let botonKits = document.getElementById('botonKits');
+let categoriaKits = document.getElementById('categoriaKits');
+
+fondo.addEventListener('click', function() {
+  if (menu.classList.contains('active')) {
+    menu.classList.remove('active');
+    fondo.classList.remove('active');
+    categoriaKits.classList.remove('active')
+    categoriaPesos.classList.remove('active')
+    categoriaMarcas.classList.remove('active')
+  } else {
+    menu.classList.add('active');
+    fondo.classList.add('active');
+  }
+});
+botonMenu.addEventListener('click', function() {
+  if (menu.classList.contains('active')) {
+    menu.classList.remove('active');
+    fondo.classList.remove('active');
+    categoriaKits.classList.remove('active')
+    categoriaPesos.classList.remove('active')
+    categoriaMarcas.classList.remove('active')
+  } else {
+    menu.classList.add('active');
+    fondo.classList.add('active');
+  }
+});
+botonPesos.addEventListener('click', function(){
+  if (categoriaPesos.classList.contains('active')) {
+    categoriaPesos.classList.remove('active')
+  } else {
+    categoriaPesos.classList.add('active')
+  }
+})
+botonMarcas.addEventListener('click', function(){
+  if (categoriaMarcas.classList.contains('active')) {
+    categoriaMarcas.classList.remove('active')
+  } else {
+    categoriaMarcas.classList.add('active')
+  }
+})
+botonKits.addEventListener('click', function(){
+  if (categoriaKits.classList.contains('active')) {
+    categoriaKits.classList.remove('active')
+  } else {
+    categoriaKits.classList.add('active')
+  }
+})
+
 categoria.addEventListener('mouseover', function() {
   boxCategorias.classList.add('active');
 });
